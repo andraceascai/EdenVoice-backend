20d17b60ca70c2df3a637393bed64402371a57f0
Ceascai Andra Maria <maceascai@gmail.com>
2024-03-13 12:27:30 +0200

    try again

--- mongodbconnect.js
+++ mongodbconnect.js
@@ -40,10 +40,6 @@ mongoose.connect(connectionString)
   app.use(bodyParser.json());
 
   //ROUTES
-  // const __dirname = path.dirname(fileURLToPath(import.meta.url))
-  // app.get('/', (req,res) => {
-  //   res.sendFile(path.join(__dirname,'hostingstart.html'))
-  // })
 
   app.get('/', async(req,res)=>{
     try {
